b5d74b1cca3995f03f679ba676c66ec601484c93
Yoon <01csyoon@gmail.com>
2026-01-06 18:40:37 +0900

    docs: 집계함수들 주석 추가

--- src/csv_processer/data_aggregator.py
+++ src/csv_processer/data_aggregator.py
@@ -1,21 +1,25 @@
 import pandas as pd
 
+# 받은 df의 row 개수 반환
 def getCountFromDF(df: pd.DataFrame):
     count = len(df)
     #print(count)
 
     return count
 
+# 받은 df에서 score column의 평균 반환
 def getAverageFromDF(df: pd.DataFrame):
     average = df['score'].mean()
     #average = df['score'].mean().round(3) 소수점 정리가 필요한 경우
     return average 
 
+# 받은 df에서 score column의 최댓값 반환
 def getMaxFromDF(df: pd.DataFrame):
     max = df['score'].max()
 
     return max
 
+# 받은 df에서 score column의 최솟값 반환
 def getMinFromDF(df: pd.DataFrame):
     min = df['score'].min()
 
