50a4997c8c5e00ded8807e97eb548e7b65c688f2
Yoon <01csyoon@gmail.com>
2026-01-06 17:24:17 +0900

    feat: argument로 받은 경로로 파일 읽기
    os 환경에 따라 /나 \쓰는게 달라서 Path로 처리

--- src/summarize.py
+++ src/summarize.py
@@ -1,15 +1,18 @@
 import argparse
+from pathlib import Path
+from csv_processer.data_loader import read_csv
 
 def main():
     # argument 읽는 부분
     parser = argparse.ArgumentParser()
-
     parser.add_argument("arg01", type=str)
-
     args = parser.parse_args()
 
-    print(f"arg: {args.arg01}")
-
+    #print(f"arg: {args.arg01}")
+    
+    path = Path(args.arg01).resolve()
+    print(path)
+    read_csv(path)
 
 if __name__ == "__main__":
     main()
