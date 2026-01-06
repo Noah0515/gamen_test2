e9ef684e7c16aa1137174aee3276fe285628405d
Yoon <01csyoon@gmail.com>
2026-01-06 18:41:38 +0900

    feat: 데이터 반환
    dictionary로 반환

--- src/csv_processer/csv_converter.py
+++ src/csv_processer/csv_converter.py
@@ -4,6 +4,7 @@ from .data_aggregator import getCountFromDF
 from .data_aggregator import getAverageFromDF
 from .data_aggregator import getMaxFromDF
 from .data_aggregator import getMinFromDF
+import json
 
 
 def csvPathToJson(path):
@@ -12,14 +13,25 @@ def csvPathToJson(path):
     #print(df)
 
     count = getCountFromDF(df)
-    print(f"count: {count}")
+    #print(f"count: {count}")
 
     average = getAverageFromDF(df)
-    print(f"average: {average}")
+    #print(f"average: {average}")
 
     max = getMaxFromDF(df)
-    print(f"max: {max}")
+    #print(f"max: {max}")
 
     min = getMinFromDF(df)
-    print(f"min: {min}")
+    #print(f"min: {min}")
+
+    # dictionary로 만들어서 반환
+    data = {
+        "count":count,
+        "average":average,
+        "max":max,
+        "min":min
+    }
+
+    #print(data)
+    return data
     
