5a7187980ed6d3515e4b0903f0dd9ef61f380a3c
Yoon <01csyoon@gmail.com>
2026-01-06 17:58:41 +0900

    feat: count함수로 목록개수 확인

--- src/csv_processer/csv_converter.py
+++ src/csv_processer/csv_converter.py
@@ -1,8 +1,13 @@
 import pandas as pd
 from .data_loader import read_csv
+from .data_aggregator import getCountFromDF
+
 
 def csvPathToJson(path):
     df = read_csv(path)
 
-    print(df)
+    #print(df)
+
+    count = getCountFromDF(df)
+    print(f"count: {count}")
     
