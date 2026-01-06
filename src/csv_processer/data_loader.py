a6623eace9e01865e8151024cf47274741bb95d0
Yoon <01csyoon@gmail.com>
2026-01-06 19:30:48 +0900

    refactor: 예외처리 로직 수정
    예외를 data_load.read_csv에서 잡지 않고 csvPathToJson을 호출하는 쪽에서
    잡도록(main에서) 수정

--- src/csv_processer/data_loader.py
+++ src/csv_processer/data_loader.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 # 파일경로에 있는 csv 파일을 읽어서 dataframe으로 돌려주는 함수
 def read_csv(file_path):
+    """
     try:
         df_file = pd.read_csv(file_path)
         #print(df_file)
@@ -10,5 +11,8 @@ def read_csv(file_path):
     except Exception as e:
         # 파일 경로가 잘못되었거나 입력받은 파일이 csv파일이 아닌경우
         print("해당 파일이 없거나 csv가 아닙니다.")
+    """
+    df_file = pd.read_csv(file_path)
+    return df_file
 
 
--- src/summarize.py
+++ src/summarize.py
@@ -11,9 +11,14 @@ def main():
     #print(f"arg: {args.arg01}")
     
     path = Path(args.arg01).resolve()
-    result = csvPathToJson(path)
-
-    print(result)
+    
+    try:
+        result = csvPathToJson(path)
+        print(result)
+    except FileNotFoundError as e:
+        print("해당 파일이 존재하지 않습니다.")
+    except Exception as e:
+        print("예상치 못한 예외가 발생했습니다.")
 
 if __name__ == "__main__":
     main()
