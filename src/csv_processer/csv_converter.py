4be25e3bb79dd461995b6e5a4b1f996c622121f8
Yoon <01csyoon@gmail.com>
2026-01-06 19:02:43 +0900

    style: 주석, import 정리

--- src/csv_processer/csv_converter.py
+++ src/csv_processer/csv_converter.py
@@ -4,7 +4,6 @@ from .data_aggregator import getCountFromDF
 from .data_aggregator import getAverageFromDF
 from .data_aggregator import getMaxFromDF
 from .data_aggregator import getMinFromDF
-import json
 
 
 def csvPathToJson(path):
@@ -32,6 +31,6 @@ def csvPathToJson(path):
         "min":min
     }
 
-    #print(data)
+    print(data)
     return data
     
